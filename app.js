4f4e3c27f3a5662e39a98d1d8ffd4bfedb6a3331
Shehroze4543 <Shehroze4543@gmail.com>
2023-06-14 22:36:37 -0400

    added some animation

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ function snow() {
   let santa = document.querySelector(".santa-div");
   let btn = document.querySelector(".snow-btn");
   let btn2 = document.querySelector(".enter-site");
+  let btn3 = document.querySelector(".enter-site-link");
   let santa_heading = document.querySelector(".santa-heading");
   santa.classList.toggle("snow-gif");
   btn.classList.add("hidden");
   santa_heading.textContent =
     "Hello there! Please click 'Enter the site' button to proceed";
   btn2.classList.remove("hidden");
+  btn3.classList.remove("hidden");
 }
